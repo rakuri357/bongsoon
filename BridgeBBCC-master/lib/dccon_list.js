0438c9196b5fc08696ad6cf54a4aef99fd7c42d4
rakuri357 <rakuri53@gmail.com>
2022-10-05 19:03:02 +0900

    bbcc

---
+++ BridgeBBCC-master/lib/dccon_list.js
@@ -0,0 +1,20 @@
+﻿dcConsData = [
+	{name:"관짝댄스.gif", keywords:["관짝댄스"], tags:["봉순"]},
+	{name:"관짝댄스2.gif", keywords:["관짝댄스2"], tags:["봉순"]},
+	{name:"관짝댄스.gif", keywords:["관짝실패"], tags:["봉순"]},
+	{name:"관짝댄스.gif", keywords:["관짝준비"], tags:["봉순"]},
+	{name:"끄덕.gif", keywords:["끄덕"], tags:["봉순"]},
+	{name:"흠.png", keywords:["흠"], tags:["봉순"]},
+
+	{name:"봉순가소롭.png", keywords:["봉순가소롭"], tags:["봉순"]},
+	{name:"봉순경멸.png", keywords:["봉순가소롭"], tags:["봉순"]},
+	{name:"봉순급함.gif", keywords:["봉순가소롭"], tags:["봉순"]},
+	{name:"봉순땀.gif", keywords:["봉순가소롭"], tags:["봉순"]},
+	{name:"봉순무표정.png", keywords:["봉순가소롭"], tags:["봉순"]},
+	{name:"봉순최면.gif", keywords:["봉순가소롭"], tags:["봉순"]},
+	{name:"봉순최면2.gif", keywords:["봉순가소롭"], tags:["봉순"]},
+	{name:"봉순행복.png", keywords:["봉순가소롭"], tags:["봉순"]},
+	{name:"봉순화남.png", keywords:["봉순가소롭"], tags:["봉순"]},
+
+];
+
